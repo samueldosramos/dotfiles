cd21117b13bdba4e20cc26432129933e66f9b41a
Samuel Ramos <samramox@gmail.com>
2017-07-08 04:04:40 -0300

    Changed Terminal by iTerm

--- install/dock.sh
+++ install/dock.sh
@@ -4,10 +4,8 @@ dockutil --no-restart --remove all
 dockutil --no-restart --add "/Applications/Launchpad.app"
 dockutil --no-restart --add "/Applications/Google Chrome.app"
 dockutil --no-restart --add "~/Applications/Chrome Apps.localized/Profile 4 hmjkmjkepdijhoojdojkdfohbdgmmhki.app"
-dockutil --no-restart --add "/Applications/Terminal.app"
+dockutil --no-restart --add "/Applications/iTerm.app"
 dockutil --no-restart --add "/Applications/Visual Studio Code.app"
-dockutil --no-restart --add '' --type spacer --section apps --after Visual Studio Code
-dockutil --no-restart --add '~/.Trash' --view grid --display folder --allhomes
 
 killall Dock
 
