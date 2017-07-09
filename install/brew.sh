64be910e41161a25bed0438570504dfcfbe538b3
Samuel Ramos <samramox@gmail.com>
2017-07-09 01:57:58 -0300

    Refactor

--- install/brew.sh
+++ install/brew.sh
@@ -7,6 +7,10 @@ brew update
 brew upgrade
 
 
+# Scientific formulae for the Homebrew package manager
+brew tap homebrew/science
+
+
 # Install Homebrew packages
 brew install dockutil                      # Command line tool for dock
 brew install git                           # Git client (more updated)
@@ -27,39 +31,50 @@ echo "Success! Brew packages are installed."
 
 # Install Cask
 brew tap caskroom/cask
-brew tap homebrew/science
 brew tap caskroom/fonts
 brew tap caskroom/drivers
 
 
 # Install Cask packages
-#brew cask install android-file-transfer    # For Android smartphone user's
-brew cask install coconutbattery            # Battery health monitor
-brew cask install dash                      # Offline documentations
+### Browser ###
 brew cask install firefox                   # Firefox browser
 brew cask install google-chrome             # Chrome browser
-brew cask install google-drive              # Google Drive client
-brew cask install imageoptim                # Images optimizer
-brew cask install iterm2                    # Better terminal for macOS
-brew cask install netbeans-php              # PHP IDE
 brew cask install opera                     # Opera browser
+
+### Torrent ###
 brew cask install qbittorrent               # Torrent client
-brew cask install spectacle                 # Window control
-brew cask install smcfancontrol             # Fan controls and monitor temperature
-brew cask install spotify                   # Spotify client
-brew cask install the-unarchiver            # Archive extraction program (RAR)
-brew cask install visual-studio-code        # Code editor
-brew cask install vlc                       # Media player
-brew cask install winbox                    # Winbox client
 brew cask install transmission              # Torrent client
+
+### Player ###
+brew cask install vlc                       # Media player
 brew cask install iina                      # Media player
+brew cask install spotify                   # Spotify client
+
+### Developer ###
+brew cask install iterm2                    # Better terminal for macOS
+brew cask install visual-studio-code        # Code editor
+brew cask install dash                      # Offline documentations
+brew cask install netbeans-php              # PHP IDE
+brew cask install imageoptim                # Images optimizer
 brew cask install boostnote                 # Note-taking for programmers
 brew cask install macdown                   # Markdown editor
-brew cask install dropbox                   # Dropbox
-brew cask install appcleaner                # App uninstall
-brew cask install logitech-options          # Logitech device customizer
-brew cask install font-fira-code            # Fira Code font
 
+### Cloud ###
+brew cask install google-drive              # Google Drive client
+brew cask install dropbox                   # Dropbox client
+
+### System ###
+brew cask install android-file-transfer     # For Android smartphone user's
+brew cask install appcleaner                # App uninstall
+brew cask install coconutbattery            # Battery health monitor
+brew cask install smcfancontrol             # Fan controls and monitor temperature
+brew cask install spectacle                 # Window control
+brew cask install the-unarchiver            # Archive extraction program (RAR)
+brew cask install logitech-options          # Logitech device customizer
+brew cask install winbox                    # Winbox client
+
+### Fonts ###
+brew cask install font-fira-code            # Fira Code font
 
 
 # Wait a bit before moving on...
