4b6dc309965680d43ddac2b48c1ce63d4c83ba1a
Samuel Ramos <samramox@gmail.com>
2017-07-21 09:01:59 -0300

    dock.sh: changed Keep by Safari

--- install/dock.sh
+++ install/dock.sh
@@ -7,7 +7,7 @@
 dockutil --no-restart --remove all
 dockutil --no-restart --add "/Applications/Launchpad.app"
 dockutil --no-restart --add "/Applications/Google Chrome.app"
-dockutil --no-restart --add "~/Applications/Chrome Apps.localized/Profile 4 hmjkmjkepdijhoojdojkdfohbdgmmhki.app"
+dockutil --no-restart --add "/Applications/Safari.app"
 dockutil --no-restart --add "/Applications/iTerm.app"
 dockutil --no-restart --add "/Applications/Visual Studio Code.app"
 
