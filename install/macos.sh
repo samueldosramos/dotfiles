adcc40891b982b092484f7e914890590826d26c5
Samuel Ramos <samramox@gmail.com>
2017-10-06 08:29:04 -0300

    Add defaults to don't rearrange spaces

--- install/macos.sh
+++ install/macos.sh
@@ -203,6 +203,9 @@ defaults write com.apple.dashboard mcx-disabled -bool true
 # Don’t show Dashboard as a Space
 defaults write com.apple.dock dashboard-in-overlay -bool true
 
+# Don’t automatically rearrange Spaces based on most recent use
+defaults write com.apple.dock mru-spaces -bool false
+
 #-----------------------------------------------------------------------------
 
 
