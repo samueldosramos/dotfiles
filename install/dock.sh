546b3acc1e65f1d03a1d7ef4b485d3ff25fea29e
Samuel Ramos <samramox@gmail.com>
2017-11-26 05:22:36 -0200

    Updated Safari order

--- install/dock.sh
+++ install/dock.sh
@@ -7,8 +7,8 @@
 dockutil --no-restart --remove all
 dockutil --no-restart --add "/Applications/Launchpad.app"
 dockutil --no-restart --add "/Applications/Spark.app"
-dockutil --no-restart --add "/Applications/Google Chrome.app"
 dockutil --no-restart --add "/Applications/Safari.app"
+dockutil --no-restart --add "/Applications/Google Chrome.app"
 dockutil --no-restart --add "/Applications/iTerm.app"
 dockutil --no-restart --add "/Applications/Visual Studio Code.app"
 
