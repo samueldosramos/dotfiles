a0faa28acfa2276777d8824fb7d097cb9c55678d
Samuel Ramos <samramox@gmail.com>
2018-04-04 10:59:48 -0300

    Fixed Removed Reactjs code snippets extension

--- install/vscode.sh
+++ install/vscode.sh
@@ -20,7 +20,6 @@ modules="
     slevesque.vscode-multiclip
     zhuangtongfa.material-theme
     vsmobile.vscode-react-native
-    xabikos.reactsnippets
     rebornix.ruby
     mrmlnc.vscode-scss
 "
