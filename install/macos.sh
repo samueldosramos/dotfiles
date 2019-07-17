2288cfe0781609c8d3e8bb6615055a21abdb76fa
Samuel Ramos <samuelramox@outlook.com>
2019-07-17 10:43:44 -0300

    feature: remove dashboard settings

--- install/macos.sh
+++ install/macos.sh
@@ -159,7 +159,7 @@ defaults write com.apple.finder FXPreferredViewStyle -string "icnv"
 defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true
 
 ###############################################################################
-# Dock, Dashboard, and hot corners                                            #
+# Dock and hot corners                                            #
 ###############################################################################
 
 # Screen Edge Position
@@ -204,12 +204,6 @@ defaults write com.apple.dock expose-animation-duration -float 0.1
 # Don’t group windows by application in Mission Control
 # (i.e. use the old Exposé behavior instead)
 defaults write com.apple.dock expose-group-by-app -bool false
-
-# Disable Dashboard
-defaults write com.apple.dashboard mcx-disabled -bool true
-
-# Don’t show Dashboard as a Space
-defaults write com.apple.dock dashboard-in-overlay -bool true
 
 # Don’t automatically rearrange Spaces based on most recent use
 defaults write com.apple.dock mru-spaces -bool false
