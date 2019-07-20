947c72142590262754330879f703740480dbf2b9
Samuel Ramos <samuelramox@outlook.com>
2019-07-20 00:35:42 -0300

    feat: update ssh step

--- install/setup.sh
+++ install/setup.sh
@@ -54,6 +54,9 @@ seek_confirmation "Warning: This step generate SSH"
 if is_confirmed; then
   ask "Please provide an email address: " && printf "\n"
   ssh-keygen -t rsa -b 4096 -C "$REPLY"
+  ssh-add -K ~/.ssh/id_rsa
+  touch ~/.ssh/config
+  echo -e "Host *\n UseKeychain yes\n AddKeysToAgent yes\n" >> ~/.ssh/config
   e_success "Generated SSH key."
   e_warning "After finishing the installation, use copyssh command to copy the SSH key to the clipboard."
 else
