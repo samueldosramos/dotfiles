43c5b5f464c9bc2e6c4c60786025f99b01725a24
Samuel Ramos <samuelramox@outlook.com>
2019-11-15 10:52:35 -0300

    feat: remove Spark app from the dock

--- install/dock.sh
+++ install/dock.sh
@@ -13,7 +13,6 @@ brew install dockutil
 # Dock settings
 dockutil --no-restart --remove all
 dockutil --no-restart --add "/System/Applications/Launchpad.app"
-dockutil --no-restart --add "/Applications/Spark.app"
 dockutil --no-restart --add "/Applications/Safari.app"
 dockutil --no-restart --add "/Applications/Google Chrome.app"
 dockutil --no-restart --add "/Applications/iTerm.app"
