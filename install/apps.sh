6fbec2c8e80d4c594be8dd8f738acde6f1fdd784
Samuel Ramos <samuelramox@outlook.com>
2021-02-08 22:16:06 -0300

    feat: update homebrew install url

--- install/apps.sh
+++ install/apps.sh
@@ -7,7 +7,7 @@
 source ./install/utils.sh
 
 # Install Homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew update
 brew upgrade
 
