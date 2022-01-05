7a4333320ef739b0a4bed6f5411138c13d7a8295
Samuel Ramos <samuelramox@outlook.com>
2022-01-05 10:58:34 -0300

    revert: fix invalid argument message in LocalHostName command

--- scripts/hostname.sh
+++ scripts/hostname.sh
@@ -7,8 +7,8 @@ source ./scripts/utils.sh
 echo_info "Setting hostname (MacBook name)..."
 
 sudo scutil --set ComputerName "$HOSTNAME"
+sudo scutil --set LocalHostName "$HOSTNAME"
 sudo scutil --set HostName "$HOSTNAME"
-sudo scutil --set LocalHostName $HOSTNAME
 sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "$HOSTNAME"
 
 # Finish
